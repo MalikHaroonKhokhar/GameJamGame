26d9ef51e1b781a11b864436d568d6f357c33437
Malik Haroon Khokhar <malikharoonkhokhar789@gmail.com>
2024-03-10 01:34:24 +0500

    Score

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,16 +6,21 @@ public class Bullet : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    public int scoreValue = 1; 
+
+    private ScoreManager scoreManager;
+   
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        scoreManager = FindObjectOfType<ScoreManager>();
         rb.velocity= transform.up * speed;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("enemy"))
         {
+            scoreManager.IncreaseScore(scoreValue);
             Destroy(collision.gameObject);
             Destroy(gameObject);   
         }
