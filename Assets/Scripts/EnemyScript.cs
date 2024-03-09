a029a26f93ddd207a4374479ef3a52b9ac95ec55
MidnightDemise <midnightdemise123@gmail.com>
2024-03-09 18:54:08 +0500

    Bullet Instantiating done
    bullet instantiating along with rotation is smooth and proper. Shooting script is not being used.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    public float moveSpeed = 3f;
-    private float spreadVirusTimer = 4f;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float spreadVirusTimer = 4f;
+    private float offsetOfEnemyWithEachOther = 1f;
+
     private float currentTimer = 0f;
+
 
     void Update()
     {
         if(currentTimer > spreadVirusTimer)
         {
-            Instantiate(gameObject, transform.position + new Vector3(0f, 1f, 0f), Quaternion.identity);
-            Instantiate(gameObject, transform.position + new Vector3(0f, -1f, 0f), Quaternion.identity);
+            Instantiate(gameObject, transform.position + new Vector3(0f, 1f, 0f) * offsetOfEnemyWithEachOther, Quaternion.identity);
+            Instantiate(gameObject, transform.position + new Vector3(0f, -1f, 0f) * offsetOfEnemyWithEachOther, Quaternion.identity);
             currentTimer = 0f;
+            offsetOfEnemyWithEachOther += 1;
         }
         else
         {
